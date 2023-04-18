88d9aca9337ca95c5d1bea73ec86dc2c3fdf991a
SimWooHyuk <tladngur2@naver.com>
2023-04-19 01:21:26 +0900

    메뉴만들기

---
+++ JS/main4.js
@@ -0,0 +1,9 @@
+const mobileMenu = document.querySelector('.mm-menu');
+
+function viewMenu() {
+  if (mobileMenu.style.display == 'block') {
+    mobileMenu.style.display = 'none'
+  } else {
+    mobileMenu.style.display = 'block'
+  };
+};
